ffaf1981eaf86907119c9d8db939e8740d1fb293
Kunal Sharma <kunalsh5739@gmail.com>
2024-06-26 13:22:14 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<main class="max-w-screen-md px-8 py-24 mx-auto space-y-14 font-main">
+	<div class="space-y-4">
+		<h1 class="text-lg font-semibold opacity-70">Dear Community,</h1>
+
+		<p class="text-lg leading-relaxed">
+			I regret to inform you that on June 17, 2024, our bank account was
+			hacked, resulting in a significant financial loss. Due to this
+			unfortunate incident, we find ourselves unable to proceed with
+			hosting our gaming nights, which were intended to feature cash
+			prizes.<br><br>This setback has deeply impacted our ability to continue
+			this project as planned. As of now, we are pausing all activities
+			related to these gaming nights until further notice. We are actively
+			exploring options to mitigate this loss and reopen this exciting
+			initiative.<br><br>Your support thus far has been invaluable to us, and we
+			appreciate your understanding during this challenging time. We
+			remain hopeful that with the right support, we can resume our gaming
+			nights and continue to provide a platform for our community to enjoy
+			and benefit from. Thank you very much for your continued patience and support.
+		</p>
+		<br>
+		<h1 class="text-lg font-semibold opacity-70">Sincerely,<br>Osume Team.</h1>
+	</div>
+</main>
